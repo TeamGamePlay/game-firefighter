ac3974702ff98d2e907a143124aa017d93ebf0f8
Alexander <alex.quinhonez@gmail.com>
2019-10-31 12:40:57 -0300

    implementacion del agua apagando el fuego

--- FireFighters/Fighter.gd
+++ FireFighters/Fighter.gd
@@ -1,8 +1,9 @@
 extends "res://FireFighters/FireFighter.gd"
 
-onready var ground_ray = get_node("RayCast2D")
+onready var ray = get_node("RayCast2D")
 
 func _control(delta):
+	
 	$Hose.look_at(get_global_mouse_position())
 	var rot_dir = 0
 	
@@ -19,8 +20,9 @@ func _control(delta):
 	if Input.is_action_pressed("ui_down"):
 		velocity = Vector2(-speed/2, 0).rotated(rotation)
 		
-	if Input.is_action_pressed("ui_accept"):
-		shoot()
+	var is_firing = Input.is_action_pressed("ui_accept")
+	$Particles2D.emitting = is_firing
+	$Area2D/CollisionPolygon2D.disabled = not is_firing
 
 func _on_GunTimer_timeout():
 	can_shoot = true
