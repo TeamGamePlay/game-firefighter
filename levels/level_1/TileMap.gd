298e7d3065e7ee84a0e02c54cb8f6fd0336fc6cb
Cristian Espindola <cris.esroj@gmail.com>
2019-11-13 19:41:30 -0300

    agrego marcado de expancion de fuego en areas combustible

--- levels/level_1/TileMap.gd
+++ levels/level_1/TileMap.gd
@@ -1,6 +1,6 @@
 extends TileMap
 
-enum CellType {HUMO,PARED,INFLAMABLE,PISO,COMBUSTIBLE,FUEGO}
+enum CellType {MARCA,PARED,INFLAMABLE,PISO,COMBUSTIBLE,FUEGO}
 
 onready var fuego = preload("res://FireFighters/fire-particles/Particles2D.tscn")
 
@@ -20,29 +20,53 @@ func marcarExpancion():
 		
 func expandirMarca(cell, cellNextType):
 	match cellNextType:
-		CellType.FUEGO: marcarSiCorresponde(cell)
+		CellType.FUEGO: marcarSiInflamable(cell)
+		CellType.COMBUSTIBLE: marcarSICombustible(cell)
 
 # deja una marcar si las celdas vecinas
 # son inflamable o combustibles
-func marcarSiCorresponde(cell):
+func marcarSiInflamable(cell):
 	var cellNorte = Vector2(cell.x, cell.y - 1)
 	var cellSur = Vector2(cell.x, cell.y + 1)
 	var cellEste = Vector2(cell.x + 1, cell.y)
-	var cellOeste = Vector2(cell.x - 1, cell.y - 1)
+	var cellOeste = Vector2(cell.x - 1, cell.y)
 	var cellNorEste = Vector2(cell.x + 1, cell.y - 1)
-	var cellNorOeste = Vector2(cell.x -1 , cell.y + 1)
+	var cellNorOeste = Vector2(cell.x -1 , cell.y - 1)
 	var cellSurEste = Vector2(cell.x + 1, cell.y + 1)
 	var cellSurOeste = Vector2(cell.x - 1, cell.y + 1)
 	
 	var celdasVecinas = [cellNorte, cellSur, cellEste, cellOeste, cellNorEste, cellNorOeste, cellSurEste, cellSurOeste]
 	
 	for cell in celdasVecinas:
-		marcar(cell, get_cellv(cell))
+		marcarInflamable(cell, get_cellv(cell))
 	
-func marcar(cell, cellType):
+func marcarInflamable(cell, cellType):
 	match cellType:
-		CellType.INFLAMABLE: set_cellv(cell,CellType.FUEGO)   
-		
+		CellType.INFLAMABLE: set_cellv(cell,CellType.MARCA)   
+
+func marcarSICombustible(cell):
+	var cellNorte = Vector2(cell.x, cell.y - 1)
+	var cellSur = Vector2(cell.x, cell.y + 1)
+	var cellEste = Vector2(cell.x + 1, cell.y)
+	var cellOeste = Vector2(cell.x - 1, cell.y)
+	var cellNorEste = Vector2(cell.x + 1, cell.y - 1)
+	var cellNorOeste = Vector2(cell.x -1 , cell.y - 1)
+	var cellSurEste = Vector2(cell.x + 1, cell.y + 1)
+	var cellSurOeste = Vector2(cell.x - 1, cell.y + 1)
+	
+	var celdasVecinas = [cellNorte, cellSur, cellEste, cellOeste, cellNorEste, cellNorOeste, cellSurEste, cellSurOeste]
+	
+	for cell in celdasVecinas:
+		marcarCombustible(cell, get_cellv(cell)) 
+
+func marcarCombustible(cell, cellType):
+	match cellType:
+		CellType.COMBUSTIBLE: marcarC(cell,cellType)
+
+func marcarC(cell, cellType):
+	set_cellv(cell,CellType.MARCA)
+	marcarSICombustible(cell)   
+
 func expandirFuego():
 	for cell in get_used_cells():  
 		var cellNextType = get_cellv(cell)
@@ -50,19 +74,20 @@ func expandirFuego():
 
 func agregarFuego(cell, cellType):
 	match cellType:
-		CellType.FUEGO: crearFuego(cell)
+		CellType.MARCA: crearFuego(cell)
 	
 func crearFuego(cell):
 	var newFuego = fuego.instance()
-	#newFuego.position.x = 16 + 32 - cell.x
-	#newFuego.position.y = 16 + 32 * cell.y
+	newFuego.position.x = 16 + 32 - cell.x
+	newFuego.position.y = 16 + 32 * cell.y
 	
-	newFuego.position.x = 655
-	newFuego.position.y = 241
+	#newFuego.position.x = 655
+	#newFuego.position.y = 241
 	print(cell)
 	print(newFuego.position)
 	print(get_parent().name)
-	get_parent().add_child(newFuego)
+	set_cellv(cell,CellType.FUEGO)
+	#get_parent().add_child(newFuego)
 	#get_parent().crear(newFuego)
 	#world.add_children(newFuego)
 	
