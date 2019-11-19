0c38435153664736f27f5b7d9421491898e9dc3b
Cristian Espindola <cris.esroj@gmail.com>
2019-11-18 22:36:57 -0300

    mensaje game over y anulacion de movimiento al perder

--- FireFighters/Fighter.gd
+++ FireFighters/Fighter.gd
@@ -1,5 +1,7 @@
 extends "res://FireFighters/FireFighter.gd"
 
+onready var gameOver = preload("res://GameOver.tscn")
+
 onready var ray = get_node("RayCast2D")
 onready var area = get_node("AreaPlayer")
 
@@ -11,17 +13,17 @@ func _control(delta):
 	$Hose.look_at(get_global_mouse_position())
 	var rot_dir = 0
 	
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") and puedeMover:
 		rot_dir += 1
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left") and puedeMover:
 		rot_dir -= 1
 		
 	rotation += rotation_speed * rot_dir * delta
 	velocity = Vector2()
 	
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up") and puedeMover:
 		velocity = Vector2(speed, 0).rotated(rotation)
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down") and puedeMover:
 		velocity = Vector2(-speed/2, 0).rotated(rotation)
 		
 	var is_firing = Input.is_action_pressed("ui_accept")
@@ -35,7 +37,10 @@ func _on_GunTimer_timeout():
 	can_shoot = true
 
 func quitaOxigeno(actOx):
-	oxigeno = actOx
+	if(oxigeno == 0):
+		oxigeno = 0
+	else:
+		oxigeno = actOx
 	
 func quitaVida(actVida):
 	vida = actVida
@@ -51,10 +56,12 @@ func _on_EnvTimer_timeout():
 			print(g)
 			global._on_smoke()
 			if(oxigeno <= 0):
-				# aca cambien sprint por le sprint de la cara para que lo cambie.
-				# pone azul cuando se queda sin oxigeno
+				# se pone azul cuando se queda sin oxigeno
 				$CaraBombero.set_modulate(Color( 0, 0, 0.55, 1 ))
-			else:# aca cambien sprint por le sprint de la cara para que lo cambie
+				puedeMover = false
+				var newGameOver = gameOver.instance()
+				get_parent().add_child(newGameOver)
+			else:
 				$CaraBombero.set_modulate(Color(r,g,b))
 			print("Pierdo oxigeno!")
 			
@@ -67,10 +74,12 @@ func _on_EnvTimer2_timeout():
 			var r2 = $CaraBombero.get_modulate().r
 			var g2 = $CaraBombero.get_modulate().g - 0.15
 			var b2 = $CaraBombero.get_modulate().b
-			print(g2)
 			global._on_fire()
 			if(vida <= 0):
 				$CaraBombero.set_modulate(Color(131, 52, 157, 255))
+				puedeMover = false
+				var newGameOver = gameOver.instance()
+				get_parent().add_child(newGameOver)
 				# Se pone morado cuando se queda sin vida
 			else:
 				$CaraBombero.set_modulate(Color(r2,g2,b2))
