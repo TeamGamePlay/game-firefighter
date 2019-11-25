2be3acddb92d432e5d16850acc1db33b27883c0b
Cristian Espindola <cris.esroj@gmail.com>
2019-11-25 10:21:43 -0300

    implemento states

--- levels/level_3/World_3.gd
+++ levels/level_3/World_3.gd
@@ -21,3 +21,16 @@ func _on_Fighter_shoot(water, _position, _direction):
 	var b = water.instance()
 	add_child(b)
 	b.start(_position, _direction)
+
+func siguienteNivel():
+	world.siguienteNivel()
+
+func gano():
+	if(sigLevel == null):
+		var newGanaste = ganaste.instance()
+		newGanaste.world = get_parent()
+		self.add_child(newGanaste)
+	else:
+		var newSigNivel = sigNivel.instance()
+		newSigNivel.world = get_parent()
+		self.add_child(newSigNivel)
